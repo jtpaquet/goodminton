7000a9c99fca7cee776275a092c3f6bb8ac0c390
unknown <jtpaquet@gmail.com>
2023-12-13 12:00:45 -0500

    Part 3 update cart functions

--- demo/urls.py
+++ demo/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('product/', views.product, name="product"),
     path('home/', views.home, name="home"),
     path('update_item/', views.updateItem, name="update_item"),
+    path('process_order/', views.processOrder, name="process_order"),
 ]
