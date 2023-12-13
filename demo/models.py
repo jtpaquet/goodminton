79d9253fc6cfbcb8f1e0b7f6454bd9486a9b8ce0
unknown <jtpaquet@gmail.com>
2023-12-13 00:25:21 -0500

    tutorial part 3 halfway

--- demo/models.py
+++ demo/models.py
@@ -16,6 +16,8 @@ class Product(models.Model):
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=False)
     image = models.ImageField(null=True, blank=True)
+    description = models.CharField(max_length=2000, null=True)
+
 
     def __str__(self):
         return self.name
